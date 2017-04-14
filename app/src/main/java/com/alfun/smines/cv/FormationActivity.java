bc8cc5335c324fbd1c7d0a35f0d18c7fb1fa5b81
Sébastien MINES <s.mines@alfun.fr>
2017-04-14 14:56:17 +0200

    Ajout du menu "Formation"
    :+1:

--- app/src/main/java/com/alfun/smines/cv/FormationActivity.java
+++ app/src/main/java/com/alfun/smines/cv/FormationActivity.java
@@ -6,7 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.ViewAnimator;
 
 /**
  * Created by s.mines on 13/04/2017.
@@ -14,15 +19,37 @@ import android.widget.ListView;
 
 public class FormationActivity extends AppCompatActivity {
 
-    private ListView listViewFormation;
-
-    public static String[][] tabFormation = {{"Aujourd'hui - 2016", "2016 - 2013"},{"Insta - Analyste logiciel", "IUT Villetaneuse - DUT Informatique"}};
+    private ImageButton buttonPrev, buttonNext;
+    private ViewAnimator viewAnimator;
+    private Animation slide_in_left, slide_out_right;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.formation);
 
-        listViewFormation = (ListView) findViewById(R.id.listViewFormation);
+        buttonPrev = (ImageButton) findViewById(R.id.prev);
+        buttonNext = (ImageButton) findViewById(R.id.next);
+        viewAnimator = (ViewAnimator)findViewById(R.id.viewanimator);
+
+        slide_in_left = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        slide_out_right = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+
+        viewAnimator.setInAnimation(slide_in_left);
+        viewAnimator.setOutAnimation(slide_out_right);
+
+        buttonPrev.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                viewAnimator.showPrevious();
+            }});
+
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                viewAnimator.showNext();
+            }});
     }
 }
